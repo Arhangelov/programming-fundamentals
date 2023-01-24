c32dea9a322b33f08557e3f1fe0a60737a9cf847
Petyo Arhangelov <arhangelov93@gmail.com>
2023-01-25 00:50:26 +0200

    sort a solution exercise

---
+++ sortASolution.js
@@ -0,0 +1,25 @@
+function sortNumbers (numOne, numTwo, numTree) {
+
+
+    // До тук връща само най-голямото число
+    // А трябва да връща ( 3 0 -2 )
+
+    for (let i = 1; i <= 1; i++) {
+        if (numOne > numTwo && numOne > numTree) {
+            console.log (numOne);
+        }
+        
+        if (numTwo > numOne && numTwo > numTree) {
+            console.log (numTwo);
+        }
+    
+        if (numTree > numOne && numTree > numTwo) {
+            console.log (numTree);
+        }
+    }
+
+    for (let j = 1; i <= 1; i++) {
+        
+    }
+}
+sortNumbers (-2, 3, 0)
