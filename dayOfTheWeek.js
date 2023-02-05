bea5ee572b476c4488848448f14b5abb891f2de2
Petyo Arhangelov <arhangelov93@gmail.com>
2023-02-06 00:49:16 +0200

    day of the week exercise

---
+++ dayOfTheWeek.js
@@ -0,0 +1,9 @@
+function dayOfTheWeek (dayAsNum) {
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    if (dayAsNum >= 1 && dayAsNum <= 7) {
+        console.log(days[dayAsNum - 1]);
+    } else {
+        console.log ('Invalid day!');
+    }
+}
